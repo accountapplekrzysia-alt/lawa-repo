4ed224b4ec863210dce01abfc49ee1c5806aa154
accountapplekrzysia-alt <accountapplekrzysia@icloud.com>
2026-05-28 15:25:30 +0200

    Update postinstall.sh

--- firefox/control/postinstall.sh
+++ firefox/control/postinstall.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+echo "Pobieram Firefoxa..."
+mkdir -p /opt/firefox
+cd /opt/firefox
+
+curl -L "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=pl" -o firefox.tar.bz2
+tar -xf firefox.tar.bz2 --strip-components=1
+rm firefox.tar.bz2
+
 chmod +x /usr/bin/firefox
-echo "Firefox został zainstalowany! Miłego przeglądania w Systemie LawaOS!!!"
+
+echo "Firefox zainstalowany!"
